4f1c614c117839fab6c42a86d98220a9c241ee59
kartik kanugo <kartik.kanugo@vectoflow.de>
2024-01-11 21:40:16 +0530

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
-
+use std::io::prelude::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
     //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("could not connect");
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                handle_connection(&mut _stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let response = "+PONG\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
