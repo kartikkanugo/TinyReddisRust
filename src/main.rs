d1a9dae2434163f58eb4953ffeb6e16d22546341
kartik kanugo <kartik.kanugo@vectoflow.de>
2024-03-29 14:07:39 +0530

    async tokio code written 9

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,12 @@ async fn handle_connection(stream: &mut TcpStream) -> Result<(), Box<dyn std::er
     let mut buffer = Vec::with_capacity(1000);
     println!("in handle connectinos");
     loop {
-        buffer.clear(); // Clear the buffer before each read operation
         let bytes_read = stream.read(&mut buffer).await?;
         println!("bytes read first {}", bytes_read);
 
-        if bytes_read == 0 {
-            break; // End of stream
-        }
+        // if bytes_read == 0 {
+        //     break; // End of stream
+        // }
         println!("bytes read {}", bytes_read);
 
         stream.write_all(response.as_bytes()).await?; // Write response to the stream
